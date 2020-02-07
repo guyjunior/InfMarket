daf86e6b66c0df99fd02b733b9d0aaa2a7471461
guyjunior <guyjunior@iq.ufrj.br>
2020-02-07 01:20:22 -0300

    Primeira etapa

---
+++ InfMarket/Models/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InfMarket.Models;
+
+
+namespace InfMarket.Models
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ InfMarket/Models/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using InfMarket;
+using InfMarket.Controllers;
+
+namespace InfMarket.Models
+{
+    public class Produto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity]
+        [Display(Name ="Produto Id"]
+        public int ProdutoId { get; set; }
+
+
+        [Required(ErrorMessage = "Nome é obrigatório!")]
+        [StringLength(50, ErrorMessage = "Nome com excesso de caracteres!")]
+        [MinLength(2, ErrorMessage = "Nome muito curto!")]
+        [Column(TypeName = "varchar(150)"]
+        [Display(Name = "Nome"]
+        public string Nome { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "varchar(50)"]
+        public string Fabricante { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "varchar(50)"]
+        public string CodBarras { get; set; }
+
+
+        
+        public decimal Preco { get; set; }
+
+
+        
+        public int Estoque { get; set; }
+
+
+
+    }
+}
